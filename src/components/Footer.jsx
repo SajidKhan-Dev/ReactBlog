a6a1ca757d4e0e3207cd9e03017af7533746aabc
SajidKhan-Dev <softwareengineersajid@gmail.com>
2024-01-11 11:03:27 +0500

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import { FaDribbble, FaFacebook, FaLinkedin, FaTwitter } from "react-icons/fa"
+import { FaDribbble, FaFacebook, FaLinkedin } from "react-icons/fa"
 import { FaXTwitter } from "react-icons/fa6"
 
 const Footer = () => {
